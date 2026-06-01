166e38abdf9cefa19e7d1cafe0fdd10dcf87c0b4
Jose Luis Sanchez Guerra <jsancgue@emeal.nttdata.com>
2026-06-01 18:14:16 +0200

    first commit

---
+++ api/data.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  res.setHeader(Access-Control-Allow-Origin, );
+  
+  const response = await fetch(
+    httpseverisgroup-my.sharepoint.comugpersonaljsancgue_emeal_nttdata_comIQCHhdKZaDOsSIBcd82PL71QAeyqIwkbvItgCdVbj1yJo28e=Gud7S9&download=1
+  );
+  
+  const data = await response.json();
+  res.setHeader(Cache-Control, s-maxage=55);
+  res.status(200).json(data);
+}
